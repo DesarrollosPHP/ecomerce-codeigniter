07346d499fb5af98f4fb7380ebd3754eb36f0678
Manuel Cortes Crisanto <crisant_89@hotmail.com>
2016-03-10 01:16:58 -0600

    Integracion del Proyecto
    Adjunto codigo fuente

---
+++ application/views/ecomerce/view_footer.php
@@ -0,0 +1,37 @@
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2><span>PHP</span>Desarrollos</h2>
+							<p>E-Commerce Desarrollado con Framework Codeigniter.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		 
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2015 PHP Desarrollos. All rights reserved.</p>
+					
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <!--<script src="<?php echo base_url()?>lib/js/jquery.js"></script>-->
+	<script src="<?php echo base_url()?>lib/js/bootstrap.min.js"></script>  
+</body>
+</html>
---
+++ application/views/view_home.php
@@ -0,0 +1,15 @@
+<h1 class="page-header"><span class="glyphicon glyphicon-indent-left"></span> Bienvenido a Mini Pos con Ecomerce</h1>
+
+<center><br/>
+
+<img src="<?php echo base_url()?>img/Logophp.png" />
+
+<BR/>
+
+Desarrollado por: Ing. Manuel Cortes Crisanto, Te Gusta mis Aportes Sigueme en: <a href="https://www.facebook.com/pages/Programacion-en-PHP/153571761391652">Programación en PHP</a>
+
+<br/><br/><br/>
+
+
+
+
